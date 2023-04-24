0d9c822de8aa0a5fa5c6f73d3eebd0e64525f50a
Robin Johannesson <robinjohannesson2@gmail.com>
2023-04-24 17:18:09 +0200

    Fix home page for 21 card game

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -16,111 +16,21 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class CardGameController extends AbstractController
 {
-    #[Route("/card", name: "card")]
-    public function cardHome(): Response
+    #[Route("/game", name: "game")]
+    public function home(): Response
     {
-        return $this->render('card/home.html.twig');
+        return $this->render('game/home.html.twig');
     }
 
-    #[Route("/card/deck", name: "card_deck")]
-    public function cardDeck(
-        SessionInterface $session
-    ): Response {
-        /** @var Card[] */
-        $deckOfCards = $session->get('deck_of_cards', null);
-        $deck = new DeckOfCards($deckOfCards);
-        $cards = $deck->getSortedCards();
-
-        $data = [
-            "cards" => $cards,
-            "num_of_cards" => count($cards),
-        ];
-
-        return $this->render('card/deck.html.twig', $data);
+    #[Route("/game/doc", name: "game_doc")]
+    public function documentation(): Response
+    {
+        return $this->render('game/doc.html.twig');
     }
 
-    #[Route("/card/deck/shuffle", name: "card_deck_shuffle", methods: ['GET'])]
-    public function cardDeckShuffle(
-        SessionInterface $session
-    ): Response {
-        /** @var Card[] */
-        $deckOfCards = $session->get('deck_of_cards', null);
-        $deck = new DeckOfCards($deckOfCards);
-        $deck->shuffle();
-
-        $data = [
-            "cards" => $deck->getCards(),
-            "num_of_cards" => $deck->getCardCount(),
-        ];
-
-        $session->set('deck_of_cards', $deck->getCards());
-
-        return $this->render('card/shuffle.html.twig', $data);
-    }
-
-    #[Route("/card/draw", name: "card_draw")]
-    public function cardDraw(
-        SessionInterface $session
-    ): Response {
-        /** @var Card[] */
-        $deckOfCards = $session->get('deck_of_cards', null);
-        $deck = new DeckOfCards($deckOfCards);
-
-        if ($deck->isEmpty()) {
-            throw new Exception("Can not draw with an empty deck.");
-        }
-
-        $hand = new CardHand();
-
-        /** @var Card */
-        $newCard = $deck->draw();
-
-        $hand->add($newCard);
-
-        $data = [
-            "cards" => $hand->getString(),
-            "num_of_cards" => $hand->getCardCount(),
-            "deck_num_of_cards" => $deck->getCardCount(),
-        ];
-
-        $session->set('deck_of_cards', $deck->getCards());
-
-        return $this->render('card/draw.html.twig', $data);
-    }
-
-    #[Route("/card/draw/{num<\d+>}", name: "card_deck_number")]
-    public function cardDrawNumber(
-        SessionInterface $session,
-        int $num
-    ): Response {
-        /** @var Card[] */
-        $deckOfCards = $session->get('deck_of_cards', null);
-        $deck = new DeckOfCards($deckOfCards);
-
-        /** @var int */
-        $numOfCards = $deck->getCardCount();
-
-        if ($num > $numOfCards) {
-            throw new Exception("Can not draw more than '$numOfCards' cards!");
-        }
-
-        $hand = new CardHand();
-
-        for ($i = 1; $i <= $num; $i++) {
-            /** @var Card */
-            $newCard = $deck->draw();
-
-            $hand->add($newCard);
-        }
-
-        $data = [
-            "cards" => $hand->getString(),
-            "num_of_cards" => $hand->getCardCount(),
-            "deck_num_of_cards" => $deck->getCardCount(),
-        ];
-
-        $session->set('deck_of_cards', $deck->getCards());
-
-        return $this->render('card/draw.html.twig', $data);
+    #[Route("/game/init", name: "game_init")]
+    public function asd(): Response
+    {
+        return $this->render('game/doc.html.twig');
     }
 }
